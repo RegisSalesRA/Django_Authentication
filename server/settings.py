52e26594687e6c1a0e02d5350b66198099244681
RegisSalesRA <regismsales@gmail.com>
2022-02-02 05:00:21 -0300

    Tutorial started login endpoint users

--- server/settings.py
+++ server/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
-    'core'
+    'rest_framework.authtoken',
+    'core',
 ]
 
 MIDDLEWARE = [
--- server/urls.py
+++ server/urls.py
@@ -14,12 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework import routers
+
 from django.conf.urls.static import static
+from core import views
 from server import settings
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 if settings.DEBUG:
