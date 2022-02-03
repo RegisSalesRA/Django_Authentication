9e7a7b5abe1f587f477fe132fade81efb21d38a7
RegisSalesRA <regismsales@gmail.com>
2022-02-03 05:28:22 -0300

    Register User with token

--- core/serializers.py
+++ core/serializers.py
@@ -1,14 +1,18 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
+from django.contrib.auth import get_user_model
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+User = get_user_model()
+
+class Userserializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+        fields = ('id','username','password','first_name','last_name','email',)
+        extra_kwargs = {'password': {"write_only":True,'required':True}}
+    
+    def create(self,validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
--- core/views.py
+++ core/views.py
@@ -1,22 +1,14 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from core.serializers import UserSerializer, GroupSerializer
+from rest_framework.response import Response
+from rest_framework import generics
+from core.serializers import *
 
+class RegisterView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = Userserializer
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    def post(self,request):
+        serializers = Userserializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response({'error':False})
+        return Response({'error':True})    
